97137ef75900663d1d5d897a623a9dc4b152bf28
Maksim Dyakonov <dyakonov_m@ineum.ru>
2026-03-22 12:15:10 +0300

    add home page

--- trainer/views.py
+++ trainer/views.py
@@ -3,5 +3,13 @@ from django.shortcuts import render
 
 
 def home(request: HttpRequest) -> HttpResponse:
-    return render(request, "trainer/home.html")
+    # Позже значения будут подставляться из сохранённого прогресса.
+    context = {
+        "nickname": "Гость",
+        "level": 1,
+        "total_attempts": 0,
+        "total_correct": 0,
+        "correct_streak": 0,
+    }
+    return render(request, "trainer/home.html", context)
 
